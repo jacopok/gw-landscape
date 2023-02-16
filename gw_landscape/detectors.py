fd0873e9f36aca72f30a2c0fa3f50dcda9381aa1
jacopok <jacopok@gmail.com>
2023-02-16 12:01:57 +0100

    allow for moving annotation around case-by-case

--- gw_landscape/detectors.py
+++ gw_landscape/detectors.py
@@ -3,6 +3,9 @@ import numpy as np
 
 class Detector(ABC):
     
+    def __init__(self):
+        self._annotation_place = None
+    
     @property
     @abstractmethod
     def frequencies(self) -> np.ndarray:
@@ -23,17 +26,23 @@ class Detector(ABC):
     def working(self) -> bool:
         ...
 
-
     @property
     def characteristic_strain(self) -> np.ndarray:
         return np.sqrt(self.frequencies * self.psd)
     
     @property
     def annotation_place(self) -> tuple[float, float]:
+        if self._annotation_place is not None:
+            return self._annotation_place
+        
         max_strain_idx = np.argmax(self.characteristic_strain)
         
+        self._annotation_place = (self.frequencies[max_strain_idx]*1.5, self.characteristic_strain[max_strain_idx])
         if self.name == 'Virgo':
-            return (self.frequencies[max_strain_idx]/15., self.characteristic_strain[max_strain_idx])
-            
+            self._annotation_place = (self.frequencies[max_strain_idx]/15., self.characteristic_strain[max_strain_idx])
         
-        return (self.frequencies[max_strain_idx]*1.5, self.characteristic_strain[max_strain_idx])
+        return self._annotation_place
+    
+    @annotation_place.setter
+    def annotation_place(self, value):
+        self._annotation_place = value
